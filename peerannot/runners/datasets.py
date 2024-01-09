46a1363b4f59ba0d6c73d1bdf58c1ad4635e1694
AxelDubar <axel.dubar@umontpellier.fr>
2024-01-09 13:25:47 +0100

    Add flag for no task dataset. Also some comments

--- peerannot/runners/datasets.py
+++ peerannot/runners/datasets.py
@@ -26,11 +26,17 @@ datasets = click.Group(
     type=click.Path(),
 )
 @click.option(
+    "--no_task",
+    is_flag=True,
+    help="True if no task is associated with the dataset",
+    # (only an answers file as in the krippendorff example dataset)
+)
+@click.option(
     "--answers_format",
     default=0,
     type=click.INT,
     help="annotation file format",
-    # 0 == Rodriguez matrix format, 1 == JSON answers/worker format, 2 == JSON worker/answers format
+    # 0 == Rodrigues matrix format, 1 == JSON answers/worker format, 2 == JSON worker/answers format
 )
 @click.option(
     "--answers",
@@ -63,6 +69,13 @@ datasets = click.Group(
     help="path to train data",
 )
 @click.option(
+    "--test_ground_truth_format",
+    default=-1,
+    type=click.INT,
+    help="annotation file format",
+    # 0 == Rodrigues matrix format, 1 == JSON answers/worker format, 2 == JSON worker/answers format
+)
+@click.option(
     "--test_ground_truth",
     default="",
     type=click.Path(exists=False),
@@ -82,12 +95,14 @@ datasets = click.Group(
 )
 def install(
     path,
+    no_task,
     answers_format,
     answers,
     metadata,
     label_names,
     files_path,
     train_path,
+    test_ground_truth_format,
     test_ground_truth,
     test_path,
     val_path,
@@ -100,18 +115,22 @@ def install(
     pathFilename = Path(path).resolve().as_posix().split("/")[-1].split(".")[0]
 
     if pathFilename == "customDataset":
-        if train_path == "":
-            click.echo("Please provide a valid train path")
-            sys.exit(1)
-        if test_path == "":
-            click.echo("Please provide a valid test path")
-            sys.exit(1)
-        if files_path == "":
-            click.echo("Please provide a valid filenames path")
-            sys.exit(1)
+        if not no_task:
+            if train_path == "":
+                click.echo("Please provide a valid train path")
+                sys.exit(1)
+            if test_path == "":
+                click.echo("Please provide a valid test path")
+                sys.exit(1)
+            if files_path == "":
+                click.echo("Please provide a valid filenames path")
+                sys.exit(1)
         if answers == "":
             click.echo("Please provide a valid answers file")
             sys.exit(1)
+        if test_ground_truth_format == -1:
+            test_ground_truth_format = answers_format
+
         spec = importlib.util.spec_from_file_location("dataset", path)
         mydata = importlib.util.module_from_spec(spec)
         sys.modules["dataset"] = mydata
@@ -123,12 +142,14 @@ def install(
         ][0]
         df = mm[1]()
         df.setfolders(
+            no_task,
             answers_format,
             answers,
             metadata,
             label_names,
             files_path,
             train_path,
+            test_ground_truth_format,
             test_ground_truth,
             test_path,
             val_path,
