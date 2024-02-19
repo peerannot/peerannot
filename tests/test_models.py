1062bc60285e58021e908e210eb2634eb5693400
tanglef <tanguy_l@outlook.fr>
2024-02-19 13:44:17 +0100

    fix tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -36,15 +36,16 @@ def test_ns():
 def test_ds():
     from peerannot.models import Dawid_Skene as DS
 
-    expected = np.array([1, 0, 1])
-    assert all([e == y_ for e, y_ in zip(expected, y)])
-
     ds = DS(ANSWERS, n_classes=2, n_workers=4)
     ds.run(maxiter=10)
     y = ds.get_probas()
-    expected = np.array([[0.25, 0.75], [2 / 3, 1 / 3], [0, 1]])
     assert y.shape == (3, 2)
-    assert np.isclose((expected - y).sum(), 0)
+    y = ds.get_answers()
+    expected = np.array([1, 0, 1])
+    assert all([e == y_ for e, y_ in zip(expected, y)])
+
+    expected = np.array([[0.25, 0.75], [2 / 3, 1 / 3], [0, 1]])
+    assert np.isclose((expected - ds.get_probas()).sum(), 0)
 
 
 def test_wawa():
@@ -54,18 +55,14 @@ def test_wawa():
     wawa.run()
     y = wawa.get_answers()
     expected = np.array([1, 0, 1])
-    assert np.isclose(
-        (wawa.worker_score - np.array([1 / 2, 1, 1, 1 / 2])).sum(), 0
-    )
+    assert np.isclose((wawa.worker_score - np.array([1 / 2, 1, 1, 1 / 2])).sum(), 0)
     assert all([e == y_ for e, y_ in zip(expected, y)])
 
 
 def test_glad():
     from peerannot.models import GLAD
 
-    glad = GLAD(
-        ANSWERS, n_classes=2, n_workers=4, dataset=dir_toydata / "temp"
-    )
+    glad = GLAD(ANSWERS, n_classes=2, n_workers=4, dataset=dir_toydata / "temp")
     glad.run()
     y = glad.get_answers()
     expected = np.array([1, 0, 1])
@@ -74,13 +71,6 @@ def test_glad():
     assert glad.beta.shape == (3,)
     assert glad.alpha.shape == (4,)
     assert (
-        len(
-            list(
-                (dir_toydata / "temp" / "identification" / "glad").glob(
-                    "*.npy"
-                )
-            )
-        )
-        == 2
+        len(list((dir_toydata / "temp" / "identification" / "glad").glob("*.npy"))) == 2
     )
     shutil.rmtree(dir_toydata / "temp")  # cleanup
